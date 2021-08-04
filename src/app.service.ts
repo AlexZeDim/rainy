084e6c46736ad0d72b3acf84b1ce8f5556ee1625
AlexZeDim <907696+AlexZeDim@users.noreply.github.com>
2021-08-04 23:52:46 +0300

    feat(bot): utils, constants & logic

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, Logger, OnApplicationBootstrap } from '@nestjs/common';
 import * as Discord from 'discord.js';
+import { normalizeGreek, replaceDiacritics } from '@app/core/utils';
+import { diacriticsJson } from '@app/core';
 
 @Injectable()
 export class AppService implements OnApplicationBootstrap {
   private client: Discord.Client
+
+  private diacritics: Record<string, string>
 
   private readonly logger = new Logger(
     AppService.name, { timestamp: true },
@@ -12,6 +16,7 @@ export class AppService implements OnApplicationBootstrap {
 
   async onApplicationBootstrap(): Promise<void> {
     this.client = new Discord.Client();
+    this.diacritics = diacriticsJson;
     await this.client.login(process.env.discord);
     await this.bot();
   }
@@ -20,6 +25,7 @@ export class AppService implements OnApplicationBootstrap {
     this.client.on('ready', async () =>
       this.logger.log(`Logged in as ${this.client.user.tag}!`)
     )
+
     await this.client.user.setPresence({
       status: 'online',
       activity: {
@@ -27,5 +33,32 @@ export class AppService implements OnApplicationBootstrap {
         type: 'WATCHING'
       }
     });
+
+    this.client.on('guildMemberAdd', async (guild_member) => {
+      let username = guild_member.user.username;
+      username = username.toLowerCase();
+      username = username.normalize("NFD");
+      username = normalizeGreek(username);
+      username = username.replace('͜', '');
+      username = username.replace('1', 'i');
+      username = username.replace('$', 's');
+      username = username.replace(/\[.*?]/gi, '');
+      username = username.replace(/\(.*?\)/gi, '');
+      username = username.replace(/\{.*?}/gi, '');
+      username = username.replace(/[`~!@#$%^€&*()_|̅+\-=?;:'",.<>{}\[\]\\\/]/gi, '');
+      username = username.replace(/\d/g,'');
+      username = replaceDiacritics(this.diacritics, username);
+      username = username.replace(/[^a-яA-Я]/g, '');
+      const C = username.replace(/[^a-zA-Z]/g, '').length;
+      const L = username.replace(/[^а-яА-Я]/g, '').length;
+
+      (L >= C)
+        ? username = username.replace(/[^а-яА-Я]/g, "")
+        : username = username.replace(/[^a-zA-Z]/g, "")
+
+      username = username[0].toUpperCase() + username.slice(1);
+
+      await guild_member.setNickname(username);
+    })
   }
 }
